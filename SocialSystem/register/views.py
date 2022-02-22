8650a532fae17f82336778f33515463e8a878d86
InferusAnima <InferusAnima@mail.ru>
2022-02-22 18:01:43 +0300

    fixed adding user to group

--- SocialSystem/register/views.py
+++ SocialSystem/register/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import Group
 from main.models import Points
 
 
@@ -9,12 +10,11 @@ def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user = form.save()
             points = Points(user=user, points=0)
             points.save()
+            group = Group.objects.get(name='user')
+            user.groups.add(group)
             login(response, user)
         else:
             return render(response, "register/register.html", {"form": form})
