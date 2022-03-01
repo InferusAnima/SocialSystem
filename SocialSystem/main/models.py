3eff7e4e6dab367991ac5ae18b10903ab12535aa
KonstantaBit <jaykonstanta@mail.ru>
2022-03-01 06:13:46 +0300

    benefit model add

--- SocialSystem/main/models.py
+++ SocialSystem/main/models.py
@@ -31,6 +31,12 @@ class Task(models.Model):
         return reverse('post', kwargs={'post_id': self.pk})
 
 
+class Benefit(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=1024)
+    cost = models.IntegerField(default=0)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=16, blank=True)
@@ -38,3 +44,4 @@ class Profile(models.Model):
     points = models.IntegerField(default=0)
     about = models.CharField(max_length=256, blank=True)
     photo = models.ImageField(upload_to=task_directory_path, verbose_name="Фото", null=True)
+    benefits = models.ManyToManyField(Benefit, related_name="benefit", null=True, blank=True)
