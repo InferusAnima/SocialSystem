1c6575a75f9618f4bd126d1645f5f31df58f7b69
InferusAnima <InferusAnima@mail.ru>
2022-03-01 20:42:06 +0300

    fix

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate
 web: gunicorn SocialSystem.wsgi --log-file -
--- SocialSystem/settings.py
+++ SocialSystem/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = []
+django_heroku.settings(locals())
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
